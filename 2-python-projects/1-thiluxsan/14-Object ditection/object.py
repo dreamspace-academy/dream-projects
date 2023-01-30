1caa1ec9a72fd6bf780d7613f33f75b4aa94a27b
Thiluxsan-Thuruparan <thiluxsanthiluxsan2@gmail.com>
2023-01-30 11:11:14 +0530

    chore:add new python project

---
+++ 2-python-projects/1-thiluxsan/14-Object ditection/object.py
@@ -0,0 +1,52 @@
+import cv2
+
+#Opencv DNN
+net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(220,220),  scale=1/255)
+
+#Load class  List
+classes= []
+with open("dnn_model/classes.txt", "r") as file_object:
+    for class_name in file_object.readlines():
+        class_name = class_name.strip()
+        classes.append(class_name)
+
+print("Objects list")
+print(classes)
+
+#Instialize camera
+cap=cv2. VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+#FULL HD 1920 X 1080
+
+def click_button(event, x, y, flags, params):
+    if event == cv2. EVENT_LBUTTONDOWN:
+        print(x, y)
+
+#Create window
+cv2.namedWindow("Frame")
+cv2.setMouseCallback("Frame", click_button)
+while True:
+#Get frames
+ ret, frame = cap.read ()
+
+#Object Detection
+ (class_ids, scores, bboxes)=model.detect(frame)
+ for class_id, score, bbox in zip(class_ids,scores, bboxes):
+     (x, y, w, h) =bbox
+     class_name = classes[class_id]
+
+     cv2.putText(frame, class_name, (x, y - 10),cv2.FONT_HERSHEY_PLAIN, 3, (200,0, 50 ), 2)
+     cv2.rectangle(frame, (x,y), (x+ w, y + h), (200,0, 50 ), 3)
+
+
+ print("class ids",class_ids)
+ print("scores", scores)
+ print("bboxes", bboxes)
+
+
+
+ cv2.imshow("frame",frame)
+ cv2.waitKey(1)
